63188f1bc5d1f7c7bdc1fb70cc9050bb6a49efd1
unknown <gjwehnes@W829-1601.edu.cbe.ab.ca>
2013-11-27 15:00:06 -0700

    Initial commit

---
+++ src/RecursiveFactorialFunctions.java
@@ -0,0 +1,67 @@
+//FirstName LastName
+//Computer Science 10
+//2012-2013 Semester 2
+//Centennial High School
+
+public class RecursiveFactorialFunctions {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(FactorialItterative(10));
+		System.out.println(FactorialFrontRecursive(10));
+		System.out.println(FactorialTailRecursive(10));
+
+	}
+	
+	private static long FactorialItterative(int n)
+	{
+		long product = 1;
+		
+		for (int j = n; j > 1; j--)
+		{
+			product *= j;
+		}
+		
+		return product;
+	}
+	
+	private static long FactorialFrontRecursive(int n)
+	{
+		long factorialOfNMinusOne = 0;
+
+		if (n < 0)
+		{
+			//factorial for negative values is undefined!
+			 throw new IllegalArgumentException("The factorial function is not defined for negative numbers");
+		}
+		else if (n == 1 || n == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			factorialOfNMinusOne = FactorialFrontRecursive(n - 1);
+			return (factorialOfNMinusOne * n);
+		}
+	}
+	
+	private static long FactorialTailRecursive(int n)
+	{
+
+		if (n < 0)
+		{
+			//factorial for negative values is undefined!
+			 throw new IllegalArgumentException("The factorial function is not defined for negative numbers");
+		}
+		else if (n == 1 || n == 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return (n * FactorialTailRecursive(n - 1));
+		}
+	}
+	
+
+}
